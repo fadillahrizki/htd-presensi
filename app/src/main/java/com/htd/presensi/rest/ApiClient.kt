0df11ac6fd56648e4880620e69b9d2ebeaac3eb2
fadillahrizki <fadillahrizki16@gmail.com>
2022-09-22 11:25:49 +0700

    add domain base url

--- app/src/main/java/com/htd/presensi/rest/ApiClient.kt
+++ app/src/main/java/com/htd/presensi/rest/ApiClient.kt
@@ -4,7 +4,8 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
-    const val BASE_URL = "http://103.151.71.42/"
+//    const val BASE_URL = "http://103.151.71.42/"
+    const val BASE_URL = "https://sisapa.tanjungbalaikota.go.id/"
 //    const val BASE_URL = "https://api-presence.z-techno.com/"
 //    const val BASE_URL = "http://10.0.2.2:8000/"
     private var retrofit: Retrofit? = null

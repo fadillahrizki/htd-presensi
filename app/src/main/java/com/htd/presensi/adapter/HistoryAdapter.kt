1e284e7aa4c73f887f74f6c43727d8ea6a5f265f
hamzahfauzy <hamzahfauzy97@gmail.com>
2022-10-02 14:31:58 +0700

    update history adapter

--- app/src/main/java/com/htd/presensi/adapter/HistoryAdapter.kt
+++ app/src/main/java/com/htd/presensi/adapter/HistoryAdapter.kt
@@ -2,17 +2,14 @@ package com.htd.presensi.adapter
 
 import android.content.Context
 import android.content.Intent
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.htd.presensi.R
 import com.htd.presensi.activity.HistoryDetailActivity
-import com.htd.presensi.activity.ProfileActivity
 import com.htd.presensi.models.Presence
 
 class HistoryAdapter(var context: Context) : RecyclerView.Adapter<HistoryAdapter.MyViewHolder>() {
